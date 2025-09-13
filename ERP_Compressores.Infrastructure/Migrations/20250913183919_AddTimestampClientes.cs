3dc0d6881bd5a7188f7aedfe77921cd409e9aa7b
pedronet00 <stabilepedro010403@gmail.com>
2025-09-13 18:24:01 -0300

    feat: adicionando timestamp para clientes

---
+++ ERP_Compressores.Infrastructure/Migrations/20250913183919_AddTimestampClientes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP_Compressores.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimestampClientes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Clientes",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Clientes",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Clientes");
+        }
+    }
+}
